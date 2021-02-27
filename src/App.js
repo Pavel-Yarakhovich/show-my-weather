e4a3ee2d4d8b78c014a7fb276403fed1ee426767
Pavel <pavel.yarri@gmail.com>
2021-02-27 16:44:31 +0300

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, HashRouter } from 'react-router-dom';
+import { routes } from './constants/utilConstants';
+import { Home } from './containers/homeContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Route path={routes.HOME} component={Home} />
+      </HashRouter>
     </div>
   );
 }
 
-export default App;
+export default App
