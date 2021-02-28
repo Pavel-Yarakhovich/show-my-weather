703d7832f63e3ce81406de88a32dab4fd299e909
Pavel Kruglik <pavlusha.kruglik@gmail.com>
2021-02-28 13:20:39 +0300

    Header.css was replaced with Header.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-import classes from './Header.module.css';
-
 const header = () => {
   return (
-    <header className={classes.header}>
-      <h1 className={classes.Date}>25 February 2021</h1>
-      <input
-        type="text"
-        placeholder="Enter location..."
-        className={classes.Location}
-      />
+    <header className="Header">
+      <h1 className="Date">25 February 2021</h1>
+      <input type="text" placeholder="Enter location..." className="Location" />
     </header>
   );
 };
