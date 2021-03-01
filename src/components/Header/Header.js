13c1f318b0faa0acd3c318928045fb09a7dc6af9
Pavel Kruglik <pavlusha.kruglik@gmail.com>
2021-03-01 13:44:38 +0300

    focus option was added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
+// import React, { useEffect } from 'react';
 import classes from './Header.module.scss';
 
 export const Header = () => {
+  // const inputRef = React.createRef();
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // });
   return (
     <header className={classes.Header}>
       <h1 className={classes.Date}>25 February 2021</h1>
       <input
         type="search"
+        // ref={inputRef}
         placeholder="Enter location..."
         className={classes.Location}
       />
