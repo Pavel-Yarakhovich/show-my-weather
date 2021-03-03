53ba0f0211d8e3a45e4ab8bc57baafed5c8813e7
Pavel Kruglik <pavlusha.kruglik@gmail.com>
2021-03-03 14:29:34 +0300

    comments were fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,17 @@
-import React from 'react';
-// import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Header.module.scss';
 
 export const Header = () => {
-  // const inputRef = React.createRef();
-  // useEffect(() => {
-  //   inputRef.current.focus();
-  // });
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <header className={classes.Header}>
       <h1 className={classes.Date}>25 February 2021</h1>
       <input
         type="search"
-        // ref={inputRef}
+        ref={inputRef}
         placeholder="Enter location..."
         className={classes.Location}
       />
