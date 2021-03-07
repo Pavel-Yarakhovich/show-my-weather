fa1880fd6dc73c1b74d4b72fa780962bc2ee783e
Pavel <pavel.yarri@gmail.com>
2021-03-07 18:34:24 +0300

    fix styles

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,7 @@ export const convertKelvinToCelsius = (tempInKelvin) => {
 
 export const getWeatherIcon = (icon) => {
   return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+};
 
 export const momentFormat = (sec, format) => {
   return moment(new Date(sec * 1000)).format(format);
